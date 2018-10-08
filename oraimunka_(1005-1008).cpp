8ffef870cb9ed26f56124f03e0fe95ce44a9796e
S3nS3IW00 <danielgellert5@gmail.com>
2018-10-08 14:04:15 +0200

    Update and rename oraimunka_1005.cpp to oraimunka_(1005-1008).cpp

--- oraimunka_(1005-1008).cpp
+++ oraimunka_(1005-1008).cpp
@@ -6,6 +6,21 @@ int duplazzMeg(int engem){
     return engem*2;
 }
 
+bool duplazzMegPointer(int& engem){
+    engem*=2;
+    return true;
+}
+
+bool negyzet(int& engem){
+    engem*=engem;
+    return true;
+}
+
+int szamHaromszorEsSzamNegyszer(int szam, int& eredmeny){
+    eredmeny = szam*4;
+    return szam*3;
+}
+
 int main()
 {
     int elsoSzam = 10;
@@ -17,7 +32,6 @@ int main()
 
     cout << "elsoSzam cime = " << &elsoSzam << endl;
     cout << "masodikSzam cime = " << &masodikSzam << endl;
-
     cout << "----||----" << endl;
 
     masodikSzam = elsoSzam;
@@ -27,7 +41,6 @@ int main()
 
     cout << "elsoSzam cime = " << &elsoSzam << endl;
     cout << "masodikSzam cime = " << &masodikSzam << endl;
-
     cout << "----||----" << endl;
 
     duplazzMeg(masodikSzam);
@@ -37,7 +50,6 @@ int main()
 
     cout << "elsoSzam cime = " << &elsoSzam << endl;
     cout << "masodikSzam cime = " << &masodikSzam << endl;
-
     cout << "----||----" << endl;
 
     int harmadikSzam = duplazzMeg(masodikSzam);
@@ -50,12 +62,10 @@ int main()
 
     cout << "harmadikSzam = " << harmadikSzam << endl;
     cout << "harmadikSzam cime = " << &harmadikSzam << endl;
-
     cout << "----||----" << endl;
 
     cout << "elsoPointer = " << elsoPointer << endl;
     cout << "elsoPointer cime = " << &elsoPointer << endl;
-
     cout << "----||----" << endl;
 
     elsoPointer = &masodikSzam;
@@ -64,7 +74,6 @@ int main()
     cout << "masodikSzam cime = " << &masodikSzam << endl;
     cout << "elsoPointer = " << elsoPointer << endl;
     cout << "elsoPointer cime = " << &elsoPointer << endl;
-
     cout << "----||----" << endl;
 
     *elsoPointer = 120;
@@ -73,11 +82,9 @@ int main()
     cout << "masodikSzam cime = " << &masodikSzam << endl;
     cout << "elsoPointer = " << elsoPointer << endl;
     cout << "elsoPointer cime = " << &elsoPointer << endl;
-
     cout << "----||----" << endl;
 
     cout << "Ahova mutat az elsoPointer = " << *elsoPointer << endl;
-
     cout << "----||----" << endl;
 
     int tomb[10];
@@ -85,7 +92,6 @@ int main()
         cout << "Tomb " << i << ". = " << tomb[i];
         cout << "\t\t A cime = " << &tomb[i] << endl;
     }
-
     cout << "----||----" << endl;
 
     for(int i = 0; i < 10; i++){
@@ -96,6 +102,41 @@ int main()
         cout << "Tomb " << i << ". = " << tomb[i];
         cout << "\t\t A cime = " << &tomb[i] << endl;
     }
+    cout << "----||----" << endl;
+
+    cout << "masodikSzam = " << masodikSzam << endl;
+    cout << "masodikSzam cime = " << &masodikSzam << endl;
+    cout << "----||----" << endl;
+
+    duplazzMegPointer(masodikSzam);
+
+    cout << "masodikSzam = " << masodikSzam << endl;
+    cout << "masodikSzam cime = " << &masodikSzam << endl;
+    cout << "----||----" << endl;
+
+    negyzet(masodikSzam);
+
+    cout << "masodikSzam = " << masodikSzam << endl;
+    cout << "masodikSzam cime = " << &masodikSzam << endl;
+    cout << "----||----" << endl;
+
+    cout << "elsoSzam = " << elsoSzam << endl;
+    cout << "masodikSzam = " << masodikSzam << endl;
+
+    cout << "elsoSzam cime = " << &elsoSzam << endl;
+    cout << "masodikSzam cime = " << &masodikSzam << endl;
+
+    int negyedikSzam = szamHaromszorEsSzamNegyszer(masodikSzam, elsoSzam);
+
+    cout << "elsoSzam = " << elsoSzam << endl;
+    cout << "masodikSzam = " << masodikSzam << endl;
+
+    cout << "elsoSzam cime = " << &elsoSzam << endl;
+    cout << "masodikSzam cime = " << &masodikSzam << endl;
+
+    cout << "negyedikSzam = " << negyedikSzam << endl;
+    cout << "negyedikSzam cime = " << &negyedikSzam << endl;
+    cout << "----||----" << endl;
 
     cout << "Lefutottam" << endl;
     return 0;
